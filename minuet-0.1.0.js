7a476f80e05ab6c3461a6a6382d38e94dbec0b8e
Vitor Freitas <vitorfs@gmail.com>
2012-09-16 02:23:38 -0300

    added isEmpty jquery function

--- minuet-0.1.0.js
+++ minuet-0.1.0.js
@@ -1,12 +1,29 @@
-﻿$.fn.countCharacters = function (limit) {		
-	if (limit - $(this).val().length <= 0) {
-		$(this).val($(this).val().substr(0, limit));
-		$(this).closest("div.control-group").addClass("warning");
+﻿$.fn.isEmpty = function () {
+	var content = "";
+	content = $(this).val();
+	if ($.trim(content).length == 0) {
+		$(this).closest(".control-group").addClass("error");
+		var error = "";
+		error = "O campo " + $(this).closest(".control-group").children(".control-label").html() + " é de preenchimento obrigatório";
+		$(this).closest(".controls").children("span[class^=help-]").html(error);
+		return 1;
 	}
 	else {
-		$(this).closest("div.controls").children("span[class^=help-]").html((limit - $(this).val().length) + " caracteres restantes");
-		$(this).closest("div.control-group").removeClass("warning");
+		$(this).closest(".control-group").removeClass("error");
+		$(this).closest(".controls").children("span[class^=help-]").html("");
+		return 0;
 	}
+};
+
+$.fn.countCharacters = function (limit) {		
+	if (limit - $(this).val().length <= 0) {
+		$(this).val($(this).val().substr(0, limit));
+		$(this).closest(".control-group").addClass("warning");
+	}
+	else {
+		$(this).closest(".control-group").removeClass("warning");
+	}
+	$(this).closest(".controls").children("span[class^=help-]").html((limit - $(this).val().length) + " caracteres restantes");
 };
 
 $.fn.limitCharacters = function () {
